1ff741269f01f99f6e03388d4d766efc5408955c
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2024-07-04 16:41:46 +0700

    fix notion link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Image src="/logo.png" width={400} height={400} alt="AAStar Logo" />
-      <h1 className="text-4xl font-bold">AAStar</h1>
+      <div className="whitespace-nowrap">
+        <h1 className="text-4xl font-bold">AAStar</h1><h1 className="text-4xl font-bold text-emerald-500 text-justify">^</h1>
+      </div>  
       <p className="text-lg text-center mb-8">
         Leveraging the Power of Community for AA Ecosystem
       </p>
@@ -22,14 +24,14 @@ export default function Home() {
         >
           Github
         </Link>
-        <Link
+        {/* <Link
           href="https://app.dework.xyz/aastar"
           className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded"
         >
           Dework
-        </Link>
+        </Link> */}
         <Link
-          href="https://www.notion.so/aastar/AAStar-f9afb521bab84dfbb077dc3628240aa0"
+          href="https://www.notion.so/planckerdao/AAStar-Working-Docs-e8c9419d8b6b4e38a6022e52b8e1a9b4?pvs=18"
           className="bg-stone-500 hover:bg-purple-600 text-white px-4 py-2 rounded"
         >
           Notion
@@ -42,9 +44,15 @@ export default function Home() {
         </Link>        
         <Link
           href="https://AAStar.io"
-          className="bg-yellow-500 hover:bg-orange-300 text-white px-4 py-2 rounded"
+          className="bg-orange-600 hover:bg-orange-600 text-white px-4 py-2 rounded"
         >
         AAStar Official
+        </Link>    
+        <Link
+          href="https://Plancker.org"
+          className="bg-emerald-500 hover:bg-emerald-500 text-white px-4 py-2 rounded"
+        >
+        Subgroup of Plancker^
         </Link>         
       </div>
     </div>
